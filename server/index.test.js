b5a6581d0e48490bd0cece72c6c6460ba48c83e7
katthartic <katt.baum@gmail.com>
2020-04-23 21:27:08 -0400

    Reset test code to original

--- server/index.test.js
+++ server/index.test.js
@@ -7,7 +7,7 @@ const seedUsers = require('../script/users.json')
 describe('GET /users', () => {
   before(async () => {
     await db.sync({ force: true })
-    await db.models.user.bulkCreate(seedUsers).done()
+    await db.models.user.bulkCreate(seedUsers)
   })
 
   it('should return list of users', async () => {
